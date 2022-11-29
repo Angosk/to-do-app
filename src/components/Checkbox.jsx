4ea7c880639f8812d946c637143ccd5ba363f8ce
Angosk <rodriguezr3yes@gmail.com>
2022-11-29 12:35:42 -0600

    bugs fixed by Angosk

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -16,9 +16,9 @@ const Checkbox = props => {
                 opacity: 0
             }}>
             <defs>
-                <path id="todo_line" d="M21 12.3h168v0.1z"/>
-                <path id="todo_box" d="M21 12.7v5c0 1.3-1 2.3-2.3 2.3H8.3C7 20 6 19 6 17.7V7.3C6 6 7 5 8.3 5h10.4C20 5 21 6 21 7.3v5.4"/>
-                <path id="todo_check" d="M10 13l2 2 5-5" />
+                <path id="todo_line" d="M21 12.3h168"/>
+                <path id="todo_box" d="M21 12.7v5c0 1.3-1 2.3-2.3 2.3H8.3C7 20 6 19 6 17.7V7.3C6 6 7 5 8 5h10.4C20 5 21 6 21 7.3v5"/>
+                <path id="todo_check" d="M10 13l2 2 5-5"/>
                 <circle id="todo_circle" cx="13.5" cy="12.5" r="10"/>
             </defs>
         </svg>
